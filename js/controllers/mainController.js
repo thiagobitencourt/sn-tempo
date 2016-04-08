c972792e8e1de04b84394ba19ae43b34317d63f4
Thiago R. M. Bitencourt <thiago.mbitencourt@gmail.com>
2016-04-08 18:10:14 -0300

    Add 'Maxima e Mínima' contente and collapse boxes

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -2,6 +2,20 @@
 app.controller('mainController', ['$scope', 'cities', 'weatherApi', function($scope, cities, weatherApi) {
   $scope.states = cities.estados;
   $scope.waiting = false;
+
+  var maxAndMin = function() {
+    var max = {temperatura_max: null};
+    var min = {temperatura_min: 100};
+    $scope.weatherInfo.previsoes.forEach(function(pr) {
+      if (pr.temperatura_max > max.temperatura_max)
+        max = pr;
+      if (pr.temperatura_min < min.temperatura_min)
+        min = pr;
+    });
+    $scope.maxima = max;
+    $scope.minima = min;
+  }
+
   $scope.showWeather = function(){
     /* If there is no state or city selected show the error message and do not search */
     if(!$scope.state || !$scope.cityName) {
@@ -16,12 +30,14 @@ app.controller('mainController', ['$scope', 'cities', 'weatherApi', function($sc
     .then(function(result) {
       $scope.weatherInfo = result.data;
       $scope.waiting = false;
+      maxAndMin();
     },
     function(err) {
         $scope.messageErr = "Não foi possível obter as informações de tempo para a cidade desejada."
         $scope.waiting = false;
     });
   }
+
   /* Closes the error message */
   $scope.closeErr = function() {
     $scope.messageErr = false;
