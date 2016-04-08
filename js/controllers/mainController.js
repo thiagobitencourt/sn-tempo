5c446c47bc47ab33ba59d76bd0c42be9fce41109
Thiago R. M. Bitencourt <thiago.mbitencourt@gmail.com>
2016-04-08 16:18:43 -0300

    Add AdminLTE template. Angular service to consume API. Select city to show weather. Responsive desing

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,3 +1,13 @@
-app.controller('mainController', ['$scope', function($scope) {
-  $scope.hello = "Hello World!";
+app.controller('mainController', ['$scope', 'cities', 'weatherApi', function($scope, cities, weatherApi) {
+  $scope.states = cities.estados;
+  $scope.showWeather = function(){
+    weatherApi.consult($scope.cityName, $scope.cities.sigla)
+    .then(function(result) {
+      $scope.weatherInfo = result.data;
+      console.log(result.data);
+    },
+    function(err) {
+        console.log("Error on search weather for this city/state");
+    });
+  }
 }]);
