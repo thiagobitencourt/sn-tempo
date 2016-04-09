ab99ec60d18361d2f324cea8e69a26907f4f8a49
Thiago R. M. Bitencourt <thiago.mbitencourt@gmail.com>
2016-04-09 00:30:29 -0300

    Add mocks data for Blumenau city, fake data. The API is not able to load Blumenau's info at no time

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,33 +1,9 @@
 'use strict'
-app.controller('mainController', ['$scope', 'cities', 'weatherApi', function($scope, cities, weatherApi) {
+app.controller('mainController', ['$scope', 'cities', 'weatherApi', 'blMocks', function($scope, cities, weatherApi, blMocks) {
   var missingState = "Selecione corretamente um estado e uma cidade.";
   $scope.states = cities.estados;
   $scope.waiting = false;
   var favoriteErr = false;
-  var loadFavorite = function() {
-    var favorite = angular.fromJson(localStorage.getItem('favorite'));
-    if (favorite) {
-      $scope.favoriteCity = favorite.city;
-      $scope.favoriteState = favorite.state;
-      $scope.hasFavorite = true;
-
-      $scope.states.some(function(st) {
-        if (st.sigla.indexOf(favorite.state) > -1) {
-          $scope.state = st;
-          return true;
-        }
-      });
-      $scope.state.cidades.some(function(ct) {
-        if(ct.indexOf($scope.favoriteCity) > -1) {
-          $scope.cityName = ct;
-          $scope.cbFavorite = true;
-          return true;
-        }
-      });
-    }
-  }
-  loadFavorite();
-
   /* Search for the day with maximum and minimum temperature*/
   var maxAndMin = function() {
     var max = {temperatura_max: null};
@@ -47,7 +23,8 @@ app.controller('mainController', ['$scope', 'cities', 'weatherApi', function($sc
     $scope.weatherInfo.previsoes.some(function(pr) {
       if (pr.data.split(" - ")[0].indexOf("Domingo") > -1) {
         $scope.sunday = pr;
-        if ((pr.descricao.indexOf("Tempo Bom") > -1) && (pr.temperatura_max > 25)) {
+        if ((pr.descricao.indexOf("Tempo Bom") > -1 ||
+              pr.descricao.indexOf("Ensolarado") > -1 ) && (pr.temperatura_max > 25)) {
           $scope.recommend = true;
         }
         return true;
@@ -80,19 +57,66 @@ app.controller('mainController', ['$scope', 'cities', 'weatherApi', function($sc
     $scope.weatherInfo = false; //Removes any existing weather info.
     $scope.messageErr = false; //Removes any existing error message.
 
-    weatherApi.consult($scope.cityName, $scope.state.sigla)
-    .then(function(result) {
-      $scope.weatherInfo = result.data;
+    var done = function(data) {
+      console.log(angular.toJson(data));
+      $scope.weatherInfo = data;
       $scope.waiting = false;
       maxAndMin();
       recommendation();
       chartConfig();
+    }
+    /* The API does not respond for Blumenau city as expected.
+     So, is used mocks to show some random datas, as suggested */
+    if ($scope.state.sigla == blMocks.sigla && $scope.cityName == blMocks.nome) {
+      console.log('Using Mocks for Blumenau City');
+      done(blMocks);
+      return;
+    }
+
+    weatherApi.consult($scope.cityName, $scope.state.sigla)
+    .then(function(result) {
+      done(result.data);
     },
     function(err) {
         $scope.messageErr = "Não foi possível obter as informações de tempo para a cidade desejada."
         $scope.waiting = false;
     });
   }
+  /* Set the favorite state and city */
+  var configFavorite = function(favorite) {
+    $scope.states.some(function(st) {
+      if (st.sigla.indexOf(favorite.state) > -1) {
+        $scope.state = st;
+        return true;
+      }
+    });
+    $scope.state.cidades.some(function(ct) {
+      if(ct.indexOf($scope.favoriteCity) > -1) {
+        $scope.cityName = ct;
+        $scope.cbFavorite = true;
+        return true;
+      }
+    });
+    $scope.showWeather(); //Load favorite weather info
+  }
+  /* Get the favorite state/city from localStorage or the default from mocks */
+  var loadFavorite = function() {
+    var favorite = angular.fromJson(localStorage.getItem('favorite'));
+    if (favorite) {
+      $scope.favoriteCity = favorite.city;
+      $scope.favoriteState = favorite.state;
+      $scope.hasFavorite = true;
+      configFavorite(favorite);
+    } else {
+      $scope.favoriteCity = blMocks.nome;
+      $scope.favoriteState = blMocks.sigla;
+      $scope.hasFavorite = true;
+      configFavorite({state: blMocks.sigla, city: blMocks.nome});
+    }
+  }
+  //First load of the page
+  loadFavorite();
+
   /* Remos the favorite place */
   $scope.removeFavorite = function() {
     localStorage.removeItem('favorite');
@@ -101,7 +125,7 @@ app.controller('mainController', ['$scope', 'cities', 'weatherApi', function($sc
     $scope.hasFavorite = false;
     $scope.cbFavorite = false;
   }
-
+  /* Save the new favorite into the localStorage */
   $scope.setFavorite = function() {
     $scope.messageErr = false; //Removes any existing error message.
     if(!$scope.cbFavorite)
@@ -119,6 +143,7 @@ app.controller('mainController', ['$scope', 'cities', 'weatherApi', function($sc
 
   /* Closes the error message */
   $scope.closeErr = function() {
+    //If the error is after a failed favorite try, the checkbox must be set to unchecked.
     if (favoriteErr) {
       $scope.cbFavorite = false;
       favoriteErr = false;
---
+++ js/services/blMocks.js
@@ -0,0 +1,96 @@
+'use strict';
+//Mocks for Blumenau city. The server is no able to resolve the blumenau city request.
+app.factory('blMocks', function() {
+  return {
+      "nome": "Blumenau",
+      "cidade":"Blumenau -  SC",
+      "sigla": "SC",
+      "agora":
+        {
+          "data_hora":"08/04/2016 - 22:00",
+          "descricao":"Muito Nublado (noite)",
+          "temperatura":"25",
+          "umidade":"100%",
+          "visibilidade":"7,24 km",
+          "vento_velocidade":"6,44 km/h",
+          "vento_direcao":"NW",
+          "pressao":"32.712,53 mBar",
+          "pressao_status":"estável",
+          "nascer_do_sol":"6:8 am",
+          "por_do_sol":"6:0 pm",
+          "imagem":"http://developers.agenciaideias.com.br/images/tempo/27.png"
+        },
+        "previsoes":
+          [
+            {
+              "data":"Sexta - 08/04/2016",
+              "descricao":"Trovoadas",
+              "temperatura_max":"31",
+              "temperatura_min":"22",
+              "imagem":"http://developers.agenciaideias.com.br/images/tempo/4.png"
+            },
+            {
+              "data":"Sábado - 09/04/2016",
+              "descricao":"Ensolarado",
+              "temperatura_max":"30",
+              "temperatura_min":"18",
+              "imagem":"http://developers.agenciaideias.com.br/images/tempo/32.png"
+            },
+            {
+              "data":"Domingo - 10/04/2016",
+              "descricao":"Ensolarado",
+              "temperatura_max":"29",
+              "temperatura_min":"17",
+              "imagem":"http://developers.agenciaideias.com.br/images/tempo/32.png"
+            },
+            {
+              "data":"Segunda - 11/04/2016",
+              "descricao":"Ensolarado",
+              "temperatura_max":"29",
+              "temperatura_min":"17",
+              "imagem":"http://developers.agenciaideias.com.br/images/tempo/32.png"
+            },
+            {
+              "data":"Terça - 12/04/2016",
+              "descricao":"Trovoadas Isoladas",
+              "temperatura_max":"27",
+              "temperatura_min":"21",
+              "imagem":"http://developers.agenciaideias.com.br/images/tempo/47.png"
+            },
+            {
+              "data":"Quarta - 13/04/2016",
+              "descricao":"Trovoadas Isoladas",
+              "temperatura_max":"27",
+              "temperatura_min":"21",
+              "imagem":"http://developers.agenciaideias.com.br/images/tempo/47.png"
+            },
+            {
+              "data":"Quinta - 14/04/2016",
+              "descricao":"Trovoadas",
+              "temperatura_max":"27",
+              "temperatura_min":"21",
+              "imagem":"http://developers.agenciaideias.com.br/images/tempo/4.png"
+            },
+            {
+              "data":"Sexta - 15/04/2016",
+              "descricao":"Trovoadas",
+              "temperatura_max":"29",
+              "temperatura_min":"23",
+              "imagem":"http://developers.agenciaideias.com.br/images/tempo/4.png"
+            },
+            {
+              "data":"Sábado - 16/04/2016",
+              "descricao":"Trovoadas",
+              "temperatura_max":"26",
+              "temperatura_min":"22",
+              "imagem":"http://developers.agenciaideias.com.br/images/tempo/4.png"
+            },
+            {
+              "data":"Domingo - 17/04/2016",
+              "descricao":"Trovoadas",
+              "temperatura_max":"30",
+              "temperatura_min":"22",
+              "imagem":"http://developers.agenciaideias.com.br/images/tempo/4.png"
+            }]
+      }
+});
